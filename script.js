bebcd11307398c7c721e6227182546ddec892059
sjperez1 <sarahpz479@gmail.com>
2022-05-24 15:49:08 -0600

    added to js

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ function changeName() {
     document.querySelector("#editprof").innerText = "Sarah Perez";
 }
 
-function removeTopRequest() {
+function removeTopRequest(i) {
     document.querySelector("#todde").remove();
     document.querySelector("#request-badge").innerText--;
+    if(i == 1) {
+        document.querySelector("#your-badge").innerText++;
+    }
 }
 
-function removeBotRequest() {
+function removeBotRequest(i) {
     document.querySelector("#phille").remove();
     document.querySelector("#request-badge").innerText--;
+    if(i == 1) {
+        document.querySelector("#your-badge").innerText++;
+    }
 }
